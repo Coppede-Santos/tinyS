efb424fb7fd549eac0cd75749336a44e357c5924
ElPelado619 <agustinyornet@gmail.com>
2026-02-09 20:51:48 -0300

    feat: agregar and compuesto

--- src/main/java/analizadorSintactico/Parser.java
+++ src/main/java/analizadorSintactico/Parser.java
@@ -1285,6 +1285,7 @@ public class Parser {
         if(type == AND) {
             macheo(AND);
             expIgual();
+            expAndPrima();
         }else {
             if (!(type == SEMICOLON || type == COMMA || type == RIGHT_PAREN
                     || type == RIGHT_BRACKET || type == OR)) {
